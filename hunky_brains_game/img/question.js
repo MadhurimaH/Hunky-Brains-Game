3665dbe11f7128d1c9130f7185f875d7ecf06e39
Madhurima Handa <Madhurima Handa>
2019-06-14 12:29:20 +0530

    Files added by Madhurima

---
+++ hunky_brains_game/img/question.js
@@ -0,0 +1,60 @@
+var question =[
+
+
+			{
+				"question":"Geeta and Babita are playing the game of coins, In each turn, a player selects either the first or last coin from the row, removes it from the row permanently, and receives the value of coin. Both the players play optimally you need to tell the maximum sum value of coins a player can make. Geeta starts the game.",
+				"option1":"1 answer",
+				"option2":"4 answer",
+				"option3":"3 answer",
+				"option4":"2 answer",
+				"answer":"1 answer"
+
+			},
+
+			{
+				"question":"Geeta and Babita are playing the game of coins, In each turn, a player selects either the first or last coin from the row, removes it from the row permanently, and receives the value of coin. Both the players play optimally you need to tell the maximum sum value of coins a player can make. Geeta starts the game.",
+				"option1":"1 answer",
+				"option2":"4 answer",
+				"option3":"3 answer",
+				"option4":"2 answer",
+				"answer":"1 answer"
+
+			},
+
+			{
+				"question":"Geeta and Babita are playing the game of coins, In each turn, a player selects either the first or last coin from the row, removes it from the row permanently, and receives the value of coin. Both the players play optimally you need to tell the maximum sum value of coins a player can make. Geeta starts the game.",
+				"option1":"1 answer",
+				"option2":"4 answer",
+				"option3":"3 answer",
+				"option4":"2 answer",
+				"answer":"1 answer"
+
+			},
+
+
+			{
+				"question":"Geeta and Babita are playing the game of coins, In each turn, a player selects either the first or last coin from the row, removes it from the row permanently, and receives the value of coin. Both the players play optimally you need to tell the maximum sum value of coins a player can make. Geeta starts the game.",
+				"option1":"1 answer",
+				"option2":"4 answer",
+				"option3":"3 answer",
+				"option4":"2 answer",
+				"answer":"1 answer"
+
+			},
+
+			{
+				"question":"Geeta and Babita are playing the game of coins, In each turn, a player selects either the first or last coin from the row, removes it from the row permanently, and receives the value of coin. Both the players play optimally you need to tell the maximum sum value of coins a player can make. Geeta starts the game.",
+				"option1":"1 answer",
+				"option2":"4 answer",
+				"option3":"3 answer",
+				"option4":"2 answer",
+				"answer":"1 answer"
+
+			},
+
+
+
+
+			
+
+]
